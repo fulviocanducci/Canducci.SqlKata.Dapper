58145817b698a7df34065ef83491e9157b429f07
fulviocanducci <fulviocanducci@hotmail.com>
2017-10-12 12:37:00 -0300

    MySQL, SqlServer and Postgres

--- Canducci.SqlKata.Dapper.Postgres/Extensions.cs
+++ Canducci.SqlKata.Dapper.Postgres/Extensions.cs
@@ -1,14 +1,14 @@
 ﻿using SqlKata.Compilers;
 using System.Data;
 
-namespace Canducci.SqlKata.Dapper.MySql
+namespace Canducci.SqlKata.Dapper.Postgres
 {
     public static class Extensions
     {
         public static QueryBuilderDapper Build(this IDbConnection connection)
-            => new QueryBuilderDapper(connection, new MySqlCompiler());
+            => new QueryBuilderDapper(connection, new PostgresCompiler());
 
         public static QueryBuilderDapper Build(this IDbConnection connection, string table)
-            => new QueryBuilderDapper(connection, new MySqlCompiler(), table);
+            => new QueryBuilderDapper(connection, new PostgresCompiler(), table);
     }
 }
--- Canducci.SqlKata.Dapper.SqlServer/Extensions.cs
+++ Canducci.SqlKata.Dapper.SqlServer/Extensions.cs
@@ -1,14 +1,14 @@
 ﻿using SqlKata.Compilers;
 using System.Data;
 
-namespace Canducci.SqlKata.Dapper.MySql
+namespace Canducci.SqlKata.Dapper.SqlServer
 {
     public static class Extensions
     {
         public static QueryBuilderDapper Build(this IDbConnection connection)
-            => new QueryBuilderDapper(connection, new MySqlCompiler());
+            => new QueryBuilderDapper(connection, new SqlServerCompiler());
 
         public static QueryBuilderDapper Build(this IDbConnection connection, string table)
-            => new QueryBuilderDapper(connection, new MySqlCompiler(), table);
+            => new QueryBuilderDapper(connection, new SqlServerCompiler(), table);
     }
 }
